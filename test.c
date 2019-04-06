ad958b2e41a34f29685e56e7ab43632664080362
AttackOnBaron <35652516+AttackOnBaron@users.noreply.github.com>
2019-04-06 00:43:55 -0500

    Update test.c

--- test.c
+++ test.c
@@ -28,9 +28,9 @@ int main()
     #pragma omp parallel for private(i,j,k) shared(A,B,C)\num_threads(4)
 	#pragma omp for schedule(static)
     for (i = 0; i < N; ++i) {
-	    #pragma omp for schedule(static)
+	    //#pragma omp for schedule(static)
         for (j = 0; j < N; ++j) {
-		#pragma omp for schedule(static)
+		//#pragma omp for schedule(static)
             for (k = 0; k < N; ++k) {
                 C[i][j] += A[i][k] * B[k][j];
             }
