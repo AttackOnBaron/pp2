740706581aea9a7bda14752c987e304c564aaf8d
AttackOnBaron <35652516+AttackOnBaron@users.noreply.github.com>
2019-04-08 14:44:56 -0500

    Update test.c

--- test.c
+++ test.c
@@ -27,8 +27,9 @@ int main()
     gettimeofday(&tv1, &tz);
 	
     #pragma omp parallel for private(i,j,k) shared(A,B,C) //num_threads(4) 
-	{ #pragma omp for schedule(static)
-      for (i = 0; i < N; ++i) {
+    { 
+		#pragma omp for schedule(static)
+      	for (i = 0; i < N; ++i) {
 	  //#pragma omp for schedule(static)
     	  for (j = 0; j < N; ++j) {
 	      //#pragma omp for schedule(static)
@@ -36,7 +37,8 @@ int main()
                   C[i][j] += A[i][k] * B[k][j];
               }
           }
-      }}
+        }
+    }
     
 
 
